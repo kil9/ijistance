134107e1bb7eaff83a93cd1bb5d10cde4259c883
kil9 <krieiter@gmail.com>
2015-12-19 14:46:43 +0000

    Modify log level to INFO

--- ijistance/config.py
+++ ijistance/config.py
@@ -23,7 +23,7 @@ RABBITMQ_TX_URL = os.environ['RABBITMQ_BIGWIG_TX_URL']
 #rabbitmq_url = os.environ['RABBITMQ_BIGWIG_URL']
 
 LOG_FORMAT = '%(asctime)s [%(levelname)s] %(message)s'
-logging.basicConfig(stream=sys.stdout, level=logging.DEBUG, format=LOG_FORMAT)
+logging.basicConfig(stream=sys.stdout, level=logging.INFO, format=LOG_FORMAT)
 log = logging.getLogger(__name__)
 log.addHandler(LogentriesHandler(logentries_key))
 
